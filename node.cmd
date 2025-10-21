3413dc43b3be6b144c26147d1e6bfa2f0e38b0b5
iaduartec <ia@duartec.es>
2025-10-21 04:02:11 +0200

    feat: video background, kitchen imagery, theme toggle, EN i18n, cache headers, and script fixes

---
+++ node.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\kiri_\AppData\Local\nvm\v22.13.1\node.exe" %*
---
+++ npx.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\kiri_\AppData\Local\nvm\v22.13.1\node.exe" "C:\Users\kiri_\AppData\Local\nvm\v22.13.1\node_modules\npm\bin\npx-cli.js" %*
